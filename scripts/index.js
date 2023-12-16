46499720c7664414902fe0f9125c270249d81ae7
Nendrxxx <153977160+Nendrxxx@users.noreply.github.com>
2023-12-16 22:38:50 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 10, 2022',
+  birthdate: 'Dec 17, 2023',
   name: 'Putri Azahra'
 };
 
